588376326b00993cb68e86c1f89e6f483bb4a746
soocrates <rooshan.vtech@gmail.com>
2025-08-04 16:48:34 +0545

    rds: init rds

---
+++ rds_pg.tf
@@ -0,0 +1,27 @@
+module "rds" {
+  source  = "terraform-aws-modules/rds/aws"
+  version = "6.7.0"
+
+  identifier                  = local.rds.identifier
+  engine                      = local.rds.engine
+  engine_version              = local.rds.engine_version
+  family                      = local.rds.family
+  port                        = local.rds.port
+  apply_immediately           = local.rds.port
+  publicly_accessible         = local.rds.apply_immediately
+  create_db_subnet_group      = local.rds.create_db_subnet_group
+  manage_master_user_password = local.rds.manage_master_user_password
+  skip_final_snapshot         = local.rds.skip_final_snapshot
+
+  instance_class      = local.rds.instance_class
+  allocated_storage   = local.rds.allocated_storage
+  deletion_protection = local.rds.deletion_protection
+
+  username = local.rds.username
+  password = local.rds.password
+
+  subnet_ids             = local.rds.subnet_ids
+  vpc_security_group_ids = local.rds.vpc_security_group_ids
+  db_subnet_group_name   = local.rds.db_subnet_group_name
+
+}
