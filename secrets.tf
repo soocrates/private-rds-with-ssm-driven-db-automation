139b751696f382e3f9fc1e1af94cae095801271d
soocrates <rooshan.vtech@gmail.com>
2025-08-04 16:59:08 +0545

    secrets: secrets to store master username and password along with db_host

--- secrets.tf
+++ secrets.tf
@@ -9,3 +9,19 @@ resource "random_pet" "master" {
   prefix    = "dbuser"
   separator = "_"
 }
+
+module "master_secret" {
+  source  = "terraform-aws-modules/secrets-manager/aws"
+  version = "1.3.1"
+
+  name_prefix             = "${naming_prefix}-masterdb-secrets"
+  description             = "Master DB credentials"
+  recovery_window_in_days = 7
+  enable_rotation         = false
+
+  secret_string = jsonencode({
+    username = random_pet.master.id
+    password = random_password.master.result
+    db_host  = module.rds.db_instance_endpoint
+  })
+}
