ae305736b772a81e199c0092427f8b1135ff7a6c
soocrates <rooshan.vtech@gmail.com>
2025-08-04 16:49:39 +0545

    rds: pass rds values via locals

--- locals.tf
+++ locals.tf
@@ -15,4 +15,28 @@ locals {
     single_nat_gateway     = true
     one_nat_gateway_per_az = false
   }
+
+  rds = {
+    identifier                  = "${naming_prefix}-rds-pg"
+    engine                      = "postgres"
+    engine_version              = "14"
+    family                      = "postgres14"
+    port                        = "5432"
+    apply_immediately           = true
+    publicly_accessible         = false
+    create_db_subnet_group      = false
+    manage_master_user_password = false
+    skip_final_snapshot         = true
+
+    instance_class      = "db.t3.micro"
+    allocated_storage   = "10"
+    deletion_protection = false
+
+    username = random_pet.master.id
+    password = random_password.master.result
+
+    subnet_ids             = [for subnet_id in module.vpc.private_subnets : subnet_id]
+    vpc_security_group_ids = [aws_security_group.rds_sg.id]
+    db_subnet_group_name   = module.vpc.database_subnet_group
+  }
 }
