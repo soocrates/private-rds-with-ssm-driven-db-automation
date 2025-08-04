5bc0659f663dbb11cbe4f2e8367ff553cc8c2dd6
soocrates <rooshan.vtech@gmail.com>
2025-08-04 15:59:16 +0545

    initialize terraform with aws provider and version ~> 5.0

---
+++ locals.tf
@@ -0,0 +1,9 @@
+locals {
+  tags = {
+    environment     = "development"
+    operationsOwner = "DevOps"
+    project         = "agrow"
+    createdBy       = "roshan.poudel"
+    terraform       = true
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  description = "The AWS region in which resources will be deployed (e.g., us-east-1)."
+  type        = string
+  default     = "us-east-1"
+}
