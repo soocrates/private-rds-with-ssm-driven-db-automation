581d899b0f3255596c3b5d50024de07158a326f4
soocrates <rooshan.vtech@gmail.com>
2025-08-05 22:15:22 +0545

    fix: removed data block to fetch ami dynamically, passed it manually

--- main.tf
+++ main.tf
@@ -1,20 +1,5 @@
-data "aws_ami" "ubuntu_latest" {
-  most_recent = true
-  owners      = ["099720109477"]
-  filter {
-    name = "name"
-    values = [
-      "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"
-    ]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 resource "aws_instance" "rds_connector" {
-  ami                         = data.aws_ami.ubuntu_latest.id
+  ami                         = "ami-020cba7c55df1f615"
   instance_type               = "t2.micro"
   subnet_id                   = module.vpc.private_subnets[0]
   vpc_security_group_ids      = [aws_security_group.rds_connector_ec2_sg.id]
@@ -26,7 +11,9 @@ resource "aws_instance" "rds_connector" {
     volume_type           = "gp3"
     delete_on_termination = true
   }
-
+  tags = {
+    Name= "${local.naming_prefix}-rds-connector"
+  }
   depends_on = [module.rds]
 }
 
