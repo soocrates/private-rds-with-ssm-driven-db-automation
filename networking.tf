f2dc9eb0eedb9c25eae90e8fc8ac3fae7b721d2c
soocrates <rooshan.vtech@gmail.com>
2025-08-05 14:36:32 +0545

    iam: added iam to allow secrets from ec2 and fix tf version along with vpc

--- networking.tf
+++ networking.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  version = "6.0.0"
+  version = "6.0.1"
 
   name = "${local.naming_prefix}-vpc"
   cidr = local.vpc.vpc_cidr
@@ -27,4 +27,27 @@ resource "aws_security_group" "rds_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_security_group" "rds_connector_ec2_sg" {
+  name        = "${local.naming_prefix}-rds-connector-sg"
+  description = "Allows outbound for SSM and RDS access"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group_rule" "rds_allow_from_ec2" {
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.rds_connector_ec2_sg.id
+  security_group_id        = aws_security_group.rds_sg.id
+  description              = "Allow PostgreSQL traffic from EC2 to RDS"
 }
