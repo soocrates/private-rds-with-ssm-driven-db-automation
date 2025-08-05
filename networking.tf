1df41650052d942f9472a6a8a6bb5beb783845dc
soocrates <rooshan.vtech@gmail.com>
2025-08-05 17:21:29 +0545

    fix: remove create db subnet group which was created problem

--- networking.tf
+++ networking.tf
@@ -9,9 +9,8 @@ module "vpc" {
   private_subnets = [for k, v in local.vpc.azs : cidrsubnet(local.vpc.vpc_cidr, 4, k)]
   public_subnets  = [for k, v in local.vpc.azs : cidrsubnet(local.vpc.vpc_cidr, 4, k + 4)]
 
-  single_nat_gateway           = true
-  one_nat_gateway_per_az       = false
-  create_database_subnet_group = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
 
   tags = local.tags
 }
